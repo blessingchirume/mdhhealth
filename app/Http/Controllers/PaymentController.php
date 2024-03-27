47a7e5f3c928b9d7eb507eacd2545bc3c6c4cd28
blessing-chirume <chirume37@gmail.com>
2024-03-27 18:53:34 +0200

    minor changes

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -21,7 +21,7 @@ class PaymentController extends Controller
 
     public function __construct()
     {
-        // $this->sapService = new SapService();
+        $this->sapService = new SapService();
         $this->middleware('menu');
     }
 
