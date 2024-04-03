4a904ca2a7e1368c02f28b1b301b7f6cae41ef33
htjena <htjena@outlook.com>
2024-04-03 21:23:58 +0200

    OPD-Queue-Updates

--- app/Http/Livewire/AddPatientModal.php
+++ app/Http/Livewire/AddPatientModal.php
@@ -6,6 +6,13 @@ use Livewire\Component;
 use App\Models\Patient;
 use App\Models\Episode;
 use App\Models\ChargeSheet;
+use App\Models\MedicalAid;
+use App\Models\MedicalAidPackage;
+use App\Models\PatientMedicalAidEntry;
+use App\Models\PaymentOption;
+use App\Models\Ward;
+use App\Models\Gurantor;
+use App\Models\NextOfKeen;
 
 class AddPatientModal extends Component
 {
@@ -21,13 +28,67 @@ class AddPatientModal extends Component
     public $nextOfKinDetails;
     public $currentStage = 1;
     public $paymentOption;
-
+    public $national_id;
+    public $email;
+    public $phone;
+    public $address;
+    public $medicalAidPolicyNumber;
+    public $medicalAidMemberName;
+    public $guarantorName;
+    public $guarantorPhone;
+    public $guarantorEmail;
+    public $guarantorAddress;
+    public $guarantorRelationship;
+    public $guarantorNationalId;
+    public $guarantorGender;
+    public $guarantorSurname;
+    
     protected $rules = [
         'name' => 'required',
         'surname' => 'required',
         'dob' => 'required',
         'gender' => 'required',
+        'email' => 'required',
+        'phone' => 'required',
+        'address' => 'required',
+        'national_id' => 'required',
+        /*'paymentMethod'=>'required',
+        'medicalAidProvider'=>'required',
+        'medicalAidPackege'=>'required',
+        'medicalAidSuffix'=>'required',
+        'medicalAidMemberName'=>'required',
+        'medicalAidPolicyNumber'=>'required'*/
     ];
+
+    public $medicalAidProviders;
+    public $medicalAidProvider;
+    public $medicalAidPackages = [];
+    public $medicalAidPackage;
+    public $paymentOptions;
+    public $visitPurpose;
+    public $otherVisitPurpose;
+    public $ward;
+    public $wards;
+    public $medicalAidSuffix;
+
+    public function mount()
+    {
+        // Fetch existing patients or set defaults
+        // Fetch medical aid providers
+        $this->medicalAidProviders = MedicalAid::all();
+        $this->paymentOptions = PaymentOption::all();
+        $this->wards = Ward::all();
+    }
+
+    public function updatedMedicalAidProvider($value)
+    {
+        // Fetch medical aid packages based on the selected provider
+        if ($value) {
+            $this->medicalAidPackages = MedicalAidPackage::where('medical_aid_id', $value)->get();
+        } else {
+            $this->medicalAidPackages = [];
+        }
+    }
 
     public function render()
     {
@@ -39,17 +100,24 @@ class AddPatientModal extends Component
         } else {
             $this->existingPatients = [];
         }
-
-        return view('livewire.add-patient-modal');
+        $medicalAidProviders = MedicalAid::all();
+        return view('livewire.add-patient-modal', compact('medicalAidProviders'));
     }
+    public function submitFocusedForm()
+    {
 
+    }
     public function resetForm()
     {
         $this->name = '';
         $this->surname = '';
-        $this->dob = null;
+        $this->dob = '';
         $this->gender = '';
         $this->search = '';
+        $this->address = '';
+        $this->phone = '';
+        $this->national_id = '';
+        $this->email = '';
         $this->selectedPatientId = null;
         $this->medicalAidDetails = '';
         $this->paymentGuarantorDetails = '';
@@ -59,8 +127,6 @@ class AddPatientModal extends Component
 
     public function nextStage()
     {
-        $this->validate();
-
         $this->currentStage++;
     }
 
@@ -74,6 +140,11 @@ class AddPatientModal extends Component
         $this->currentStage++;
     }
 
+    public function selectMedicalAid($provider)
+    {
+        $this->medicalAidPackages = MedicalAid::find($provider)->with('packages')->get();
+    }
+
     public function selectPatient($patientId)
     {
         // Fetch the details of the selected patient
@@ -84,6 +155,11 @@ class AddPatientModal extends Component
         $this->surname = $selectedPatient->surname;
         $this->dob = $selectedPatient->dob;
         $this->gender = $selectedPatient->gender;
+        $this->address = $selectedPatient->address;
+        $this->phone = $selectedPatient->phone;
+        $this->email = $selectedPatient->email;
+        $this->national_id = $selectedPatient->national_id;
+
 
         // Clear the search input and existing suggestions
         $this->search = '';
@@ -92,40 +168,120 @@ class AddPatientModal extends Component
 
     public function addPatient()
     {
-        // If a patient is selected from the autosuggestion
-        if ($this->selectedPatientId) {
-            // Add selected patient to OPD queue
-            Episode::create([
+        // Create a new patient
+        try {
+            if ($this->selectedPatientId == null) {
+                $newPatient = Patient::create([
+                    'name' => $this->name,
+                    'surname' => $this->surname,
+                    'dob' => $this->dob,
+                    'gender' => $this->gender,
+                    'address' => $this->address,
+                    'phone' => $this->phone,
+                    'email' => $this->email,
+                    'national_id' => $this->national_id,
+                    'patient_id' => 'MDHP' . rand(00000, 99999),
+                ]);
+
+                $this->selectedPatientId = $newPatient->id;
+
+                session()->flash('success', 'Patient added successfully!');
+                $this->nextStage();
+            } else {
+                $this->queuePatient();
+            }
+        } catch (\Exception $e) {
+            session()->flash('error', 'Error adding patient: ' . $e->getMessage());
+        }
+    }
+
+    public function addMedicalAid()
+    {
+        $this->validate([
+            'medicalAidProvider' => 'required',
+            'medicalAidPackage' => 'required',
+            'medicalAidSuffix' => 'required',
+            'medicalAidMemberName' => 'required',
+            'medicalAidPolicyNumber' => 'required'
+        ]);
+
+        try {
+            $medicalAidEntry = PatientMedicalAidEntry::create([
                 'patient_id' => $this->selectedPatientId,
-                // Other fields for OPD queue
-            ]);
-        } else {
-            // Create a new patient
-            $newPatient = Patient::create([
-                'name' => $this->name,
-                'surname' => $this->surname,
-                'dob' => $this->dob,
-                'gender' => $this->gender,
-                // Other fields for Patient
+                'suffix_number' => $this->medicalAidSuffix,
+                'package_id' => $this->medicalAidPackage,
+                'member_name' => $this->medicalAidMemberName,
+                'policy_number' => $this->medicalAidPolicyNumber,
             ]);
 
-            // Add new patient to OPD queue
-            $data["episode_entry"] = (int)Episode::where('patient_id', $newPatient->id)->max('episode_entry') + 1;
-            $data["episode_code"] = $newPatient->patient_id . "/" . $data["episode_entry"];
+            session()->flash('success', 'Medical aid added successfully!');
+            $this->nextStage();
 
-            $data["patient_id"] = $newPatient->id;
-            $data["payment_option_id"] = $this->paymentOption;
-            $data["date"] = date('Y-m-d');
-
-            $episode = Episode::create($data);
-
-            ChargeSheet::create([
-                "episode_id" => $episode->id,
-                "checkin" => date('Y-m-d'),
-            ]);
+        } catch (\Exception $e) {
+            session()->flash('error', 'Error adding medical aid: ' . $e->getMessage());
         }
 
-        // Reset the form
+    }
+
+    public function addGuarantor()
+    {
+        $this->validate([
+            'guarantorName' => 'required',
+            'guarantorSurname' => 'required',
+            'guarantorPhone' => 'required',
+            'guarantorGender' => 'required',
+            'guarantorAddress' => 'required',
+            'guarantorRelationship' => 'required',
+            'guarantorNationalId' => 'required',
+        ]);
+
+       // try {
+            $guarantor = Gurantor::create([
+                'patient_id' => $this->selectedPatientId,
+                'name' => $this->guarantorName,
+                'phone' => $this->guarantorPhone,
+                'surname' => $this->guarantorSurname,
+                'address' => $this->guarantorAddress,
+                'relationship' => $this->guarantorRelationship,
+                'national_id' => $this->guarantorNationalId,
+                'gender' => $this->guarantorGender,
+            ]);
+
+            session()->flash('success', 'Guarantor added successfully!');
+            $this->nextStage();
+      //  } catch (\Exception $e) {
+      //      session()->flash('error', 'Error adding guarantor: ' . $e->getMessage());
+      //  }
+        $this->resetGuarantorFields();
+    }
+
+    public function resetGuarantorFields()
+    {
+        $this->guarantorName = '';
+        $this->guarantorPhone = '';
+        $this->guarantorEmail = '';
+        $this->guarantorAddress = '';
+        $this->guarantorRelationship = '';
+    }
+    public function queuePatient()
+    {
+        $data["episode_entry"] = (int) Episode::where('patient_id', $this->selectedPatientId)->max('episode_entry') + 1;
+        $data["episode_code"] = $this->selectedPatientId . "/" . $data["episode_entry"];
+
+        $data["patient_id"] = $this->selectedPatientId;
+        $data["patient_type"] = 'OutPatient';
+        $data["payment_option_id"] = $this->paymentOption;
+        $data['visit_purpose'] = ($this->visitPurpose != 'other') ? $this->visitPurpose : $this->otherVisitPurpose;
+        $data['ward'] = $this->ward;
+        $data["date"] = date('Y-m-d');
+
+        $episode = Episode::create($data);
+
+        ChargeSheet::create([
+            "episode_id" => $episode->id,
+            "checkin" => date('Y-m-d'),
+        ]);
+
         $this->resetForm();
 
         // Close the modal after adding the patient
--- resources/views/livewire/add-patient-modal.blade.php
+++ resources/views/livewire/add-patient-modal.blade.php
@@ -1,81 +1,241 @@
 <div class="modal-body">
-    <ul class="nav nav-tabs">
+    <ul class="nav nav-tabs" style="cursor:disabled;">
         <li class="nav-item">
-            <a class="nav-link {{ $currentStage == 1 ? 'active' : '' }}" wire:click="switchStage(1)">Patient Details</a>
+            <a class="nav-link {{ $currentStage == 1 ? 'active' : '' }}">Patient Details</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link {{ $currentStage == 2 ? 'active' : '' }}" wire:click="switchStage(2)">Medical Aid</a>
+            <a class="nav-link {{ $currentStage == 2 ? 'active' : '' }}">Payment Method</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link {{ $currentStage == 3 ? 'active' : '' }}" wire:click="switchStage(3)">Payment Guarantor</a>
+            <a class="nav-link {{ $currentStage == 3 ? 'active' : '' }}">Payment
+                Guarantor</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link {{ $currentStage == 4 ? 'active' : '' }}" wire:click="switchStage(4)">Next of Kin</a>
+            <a class="nav-link {{ $currentStage == 4 ? 'active' : '' }}">Next of Kin</a>
         </li>
     </ul>
     <div class="tab-content mt-2">
         <div class="tab-pane fade {{ $currentStage == 1 ? 'show active' : '' }}" id="patient-details" role="tabpanel">
             <!-- Patient Details Form Section -->
-            <form wire:submit.prevent="nextStage">
+            <form wire:submit.prevent="addPatient">
                 <!-- Patient Details Form Fields -->
                 <div class="form-group">
                     <label for="search">Search Patient</label>
                     <input type="text" class="form-control" wire:model.debounce.300ms="search" id="search">
                     <div class="mt-2">
-                        @foreach($existingPatients as $patient)
-                            <div
-                                wire:click="selectPatient({{ $patient->id }})"
-                                class="cursor-pointer suggestion-item"
-                            >
+                        @foreach ($existingPatients as $patient)
+                            <div wire:click="selectPatient({{ $patient->id }})" class="cursor-pointer suggestion-item">
                                 {{ $patient->name }} {{ $patient->surname }}
                             </div>
                         @endforeach
                     </div>
                 </div>
-                <div class="form-group">
-                    <label for="name">Name</label>
-                    <input type="text" class="form-control" wire:model.defer="name" id="name">
+                <div class="row">
+                    <div class="form-group col-md-4">
+                        <label for="name">Name</label>
+                        <input type="text" class="form-control" wire:model.defer="name" id="name">
+                    </div>
+                    <div class="form-group col-md-4">
+                        <label for="surname">Surname</label>
+                        <input type="text" class="form-control" wire:model.defer="surname" id="surname">
+                    </div>
+                    <div class="form-group col-md-2">
+                        <label for="age">D.O.B</label>
+                        <input type="date" class="form-control" wire:model.defer="dob" id="dob">
+                    </div>
+                    <div class="form-group col-md-2">
+                        <label for="gender">Gender</label>
+                        <select class="form-control" wire:model.defer="gender" id="gender">
+                            <option value="">Select Gender</option>
+                            <option value="male">Male</option>
+                            <option value="female">Female</option>
+                            <option value="other">Other</option>
+                        </select>
+                    </div>
                 </div>
-                <div class="form-group">
-                    <label for="surname">Surname</label>
-                    <input type="text" class="form-control" wire:model.defer="surname" id="surname">
+                <div class="row">
+                    <div class="form-group col-md-4">
+                        <label for="id_number">National ID No.</label>
+                        <input type="text" class="form-control" wire:model.defer="national_id" id="national_id">
+                    </div>
+                    <div class="form-group col-md-4">
+                        <label for="phone_number">Phone Number</label>
+                        <input type="text" class="form-control" wire:model.defer="phone" id="phone">
+                    </div>
+                    <div class="form-group col-md-4">
+                        <label for="email">Email</label>
+                        <input type="email" class="form-control" wire:model.defer="email" id="email">
+                    </div>
+                    <div class="form-group col-md-4">
+                        <label for="address">Address</label>
+                        <textarea class="form-control" wire:model.defer="address" id="address"></textarea>
+                    </div>
+                    <div class="form-group col-md-4">
+                        <label for="purpose">Purpose of Visit</label>
+                        <select class="form-control" wire:model="visitPurpose" id="purpose">
+                            <option value="">Select Purpose of Visit</option>
+                            <option value="review">Review</option>
+                            <option value="treatment">Treatment</option>
+                            <option value="other">Other</option>
+                        </select>
+                        @if ($visitPurpose == 'other')
+                            <div class="mt-2">
+                                <input type="text" class="form-control" wire:model.defer="otherVisitPurpose"
+                                    id="purpose-other" placeholder="Enter Purpose of Visit">
+                            </div>
+                        @else
+                            &nbsp;
+                        @endif
+                    </div>
+                    <div class="form-group col-md-4">
+                        <label for="ward">Ward</label>
+                        <select class="form-control" wire:model="ward" id="ward">
+                            <option value="">Select Ward</option>
+                            @foreach ($wards as $ward)
+                                <option value="{{ $ward->id }}">{{ $ward->name }}</option>
+                            @endforeach
+                        </select>
+                    </div>
                 </div>
-                <div class="form-group">
-                    <label for="age">D.O.B</label>
-                    <input type="number" class="form-control" wire:model.defer="dob" id="dob">
-                </div>
-                <div class="form-group">
-                    <label for="gender">Gender</label>
-                    <select class="form-control" wire:model.defer="gender" id="gender">
-                        <option value="">Select Gender</option>
-                        <option value="male">Male</option>
-                        <option value="female">Female</option>
-                        <option value="other">Other</option>
-                    </select>
-                </div>
-                <button type="submit" class="btn btn-primary">Next</button>
+                <button type="submit" class="btn btn-primary">Submit</button>
 
             </form>
         </div>
-        <div class="tab-pane fade {{ $currentStage == 2 ? 'show active' : '' }}" id="medical-aid" role="tabpanel">
-            <!-- Medical Aid Form Section -->
-            <form wire:submit.prevent="nextStage">
-                <!-- Medical Aid Form Fields -->
-                <!-- Medical Aid Details -->
-                <!-- Submit Button -->
+        <div class="tab-pane fade {{ $currentStage == 2 ? 'show active' : '' }}" id="payment-methods"
+            role="tabpanel">
+            <!-- Payment Methods Form Section -->
+            <div class="form-group">
+                <label for="payment-mode">Payment Mode</label>
+                <select class="form-control" wire:model="paymentOption" id="payment-mode">
+                    <option value="1">Cash</option>
+                    <option value="2">Medical Aid</option>
+                </select>
+            </div>
+            <form wire:submit.prevent="addMedicalAid">
+
+
+                @if ($paymentOption == 2)
+                    <!-- Medical Aid Form Fields -->
+                    <div class="row">
+                        <div class="form-group col-md-6">
+                            <label for="medical-aid-provider">Medical Aid Provider</label>
+                            <select class="form-control" wire:model="medicalAidProvider" id="medical-aid-provider">
+                                <option value="">Select Provider</option>
+                                @foreach ($medicalAidProviders as $medicalAid)
+                                    <option value="{{ $medicalAid->id }}">{{ $medicalAid->provider_name }}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                        @if ($medicalAidProvider && count($medicalAidPackages) > 0)
+                            <div class="form-group col-md-6">
+                                <label for="medical-aid-package">Medical Aid Package</label>
+                                <select class="form-control" wire:model="medicalAidPackage" id="medical-aid-package">
+                                    <option value="">Select Package</option>
+                                    @foreach ($medicalAidPackages as $package)
+                                        <option value="{{ $package->id }}">{{ $package->name }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+
+                            <div class="form-group col-md-4">
+                                <label for="medical-aid-suffix">Suffix</label>
+                                <input type="text" class="form-control" wire:model.defer="medicalAidSuffix"
+                                    id="medical-aid-suffix">
+                            </div>
+                            <div class="form-group col-md-4">
+                                <label for="medical-aid-member-name">Member Name</label>
+                                <input type="text" class="form-control" wire:model.defer="medicalAidMemberName"
+                                    id="medical-aid-member-name">
+                            </div>
+                            <div class="form-group col-md-4">
+                                <label for="medical-aid-policy-number">Policy Number</label>
+                                <input type="text" class="form-control" wire:model.defer="medicalAidPolicyNumber"
+                                    id="medical-aid-policy-number">
+                            </div>
+                    </div>
+                @endif
+                @endif
+                <br />
+                <div class="row">
+                    <div class="col-md-12">
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </div>
+                </div>
+
             </form>
         </div>
-        <div class="tab-pane fade {{ $currentStage == 3 ? 'show active' : '' }}" id="payment-guarantor" role="tabpanel">
+
+        <div class="tab-pane fade {{ $currentStage == 3 ? 'show active' : '' }}" id="payment-guarantor"
+            role="tabpanel">
             <!-- Payment Guarantor Form Section -->
-            <form wire:submit.prevent="nextStage">
-                <!-- Payment Guarantor Form Fields -->
-                <!-- Payment Guarantor Details -->
-                <!-- Submit Button -->
+            <form wire:submit.prevent="addGuarantor">
+                <div class="row">
+                    <div class="form-group col-md-4">
+                        <label for="guarantor_name">Guarantor Name</label>
+                        <input type="text" class="form-control" wire:model.defer="guarantorName"
+                            id="guarantor_name">
+                        @error('guarantor_name')
+                            <span class="text-danger">{{ $message }}</span>
+                        @enderror
+                    </div>
+                    <div class="form-group col-md-4">
+                        <label for="phone">Phone</label>
+                        <input type="text" class="form-control" wire:model.defer="guarantorPhone" id="phone">
+                        @error('phone')
+                            <span class="text-danger">{{ $message }}</span>
+                        @enderror
+                    </div>
+                    <div class="form-group col-md-4">
+                        <label for="email">Email</label>
+                        <input type="email" class="form-control" wire:model.defer="guarantorEmail" id="email">
+                        @error('email')
+                            <span class="text-danger">{{ $message }}</span>
+                        @enderror
+                    </div>
+                    <div class="form-group col-md-4">
+                        <label for="national_id">National ID</label>
+                        <input type="text" class="form-control" wire:model.defer="guarantorNationalId"
+                            id="national_id">
+                        @error('national_id')
+                            <span class="text-danger">{{ $message }}</span>
+                        @enderror
+                    </div>
+                    <div class="form-group col-md-4">
+                        <label for="gender">Gender</label>
+                        <select class="form-control" wire:model.defer="guarantorGender"
+                            id="gender">
+                            <option value="">Select Gender</option>
+                            <option value="male">Male</option>
+                            <option value="female">Female</option>
+                        </select>
+                        @error('gender')
+                            <span class="text-danger">{{ $message }}</span>
+                        @enderror
+                    </div>
+                    <div class="form-group col-md-4">
+                        <label for="relationship">Relationship</label>
+                        <input type="text" class="form-control" wire:model.defer="guarantorRelationship"
+                            id="relationship">
+                        @error('relationship')
+                            <span class="text-danger">{{ $message }}</span>
+                        @enderror
+                    </div>
+                    <div class="form-group col-md-8">
+                        <label for="address">Address</label>
+                        <textarea class="form-control" wire:model.defer="guarantorAddress" id="address"></textarea>
+                        @error('address')
+                            <span class="text-danger">{{ $message }}</span>
+                        @enderror
+                    </div>
+
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
             </form>
+
         </div>
         <div class="tab-pane fade {{ $currentStage == 4 ? 'show active' : '' }}" id="next-of-kin" role="tabpanel">
             <!-- Next of Kin Form Section -->
-            <form wire:submit.prevent="addPatient">
+            <form wire:submit.prevent="addNextOfKin">
                 <!-- Next of Kin Form Fields -->
                 <!-- Next of Kin Details -->
                 <!-- Submit Button -->
@@ -88,22 +248,33 @@
         @endif
 
         @if ($currentStage < 4)
-            <button type="button" class="btn btn-primary" wire:click="nextStage">Next</button>
+            <button type="submit" class="btn btn-primary" wire:click="nextStage">Next</button>
             <button type="button" class="btn btn-secondary ml-2" wire:click="skipStage">Skip</button>
         @else
-            <button type="button" class="btn btn-primary" wire:click="addPatient">Add Patient</button>
+            <button type="button" class="btn btn-success" wire:click="queuePatient">Conclude</button>
         @endif
     </div>
 </div>
 
 <style>
     .suggestion-item {
-    padding: 8px 12px;
-    cursor: pointer;
-}
+        padding: 8px 12px;
+        cursor: pointer;
+    }
 
-.suggestion-item:hover {
-    background-color: #f0f0f0;
-}
-
+    .suggestion-item:hover {
+        background-color: #f0f0f0;
+    }
 </style>
+<script>
+    document.addEventListener('livewire:load', function() {
+        // Add event listener to the "Next" button
+        document.querySelector('.modal-footer button.btn-primary').addEventListener('click', function() {
+            // Find the focused form and submit it
+            const focusedForm = document.querySelector('form:focus');
+            if (focusedForm) {
+                focusedForm.submit();
+            }
+        });
+    });
+</script>
