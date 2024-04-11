71f21d0ee3afa17402d7d55012789f3f91dae14e
blessing-chirume <chirume37@gmail.com>
2024-04-11 19:16:53 +0200

    re,moved duplicate currency seeder call

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -29,7 +29,7 @@ class DatabaseSeeder extends Seeder
             TestsSeeder::class,
             TestCategorySeeder::class,
             PaymentOptionSeeder::class,
-            CurrencySeeder::class
         ]);
     }
 }
+    
