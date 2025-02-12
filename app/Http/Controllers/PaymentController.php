49c3b3b17d23a2c1d5dc17f25d8f3e3fe80db5ab
Blessing Chirume <61548727+blessingchirume@users.noreply.github.com>
2025-02-12 17:31:26 +0200

    Implemented local payments module

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -42,63 +42,63 @@ class PaymentController extends Controller
 
     public function store(StorePaymentRequest $request)
     {
-        $data = [
-            "DocDate" => "2024-01-18",
-            "DocDueDate" => "2024-01-18",
-            "DocType" => "dDocument_Service",
-            "CardCode" => "CRA001",
-            "NumAtCard" => "Test",
-            "DocTotal" => 370000,
-            "DocRate" => 1.0,
-            "ContactPersonCode" => 0,
-            "DocObjectType" => "oInvoices",
-            "DocumentLines" => [
-                [
-                    "ItemDescription" => "Test item",
-                    "Total" => 370000,
-                    "Price" => 0,
-                    "AccountCode" => 310003,
-                    "VatGroup" => "O01",
-                    "LineTotal" => 370000,
-                    "RowTotalSC" => 0,
-                    "TaxCode" => "I1",
-                    "DiscountPercent" => 0
-                ]
-            ]
-        ];
-        $this->sapService->createSapInvoice($data);
-        // dd($request);
-        // $data = $request->validate([
-        //     'patient_type' => 'required',
-        //     // 'narration' => 'required',
-        //     'ward' => 'required',
-        // ]);
-        // $data["attendee"] = Auth::user()->name. " " . Auth::user()->surname;
-        // $data["episode_entry"] = (int)Episode::where('patient_id', $request->patient_id)->max('episode_entry') + 1;
-        // $data["episode_code"] = $request->patient_id . "/" . $data["episode_entry"];
+        // $data = [
+        //     "DocDate" => "2024-01-18",
+        //     "DocDueDate" => "2024-01-18",
+        //     "DocType" => "dDocument_Service",
+        //     "CardCode" => "CRA001",
+        //     "NumAtCard" => "Test",
+        //     "DocTotal" => 370000,
+        //     "DocRate" => 1.0,
+        //     "ContactPersonCode" => 0,
+        //     "DocObjectType" => "oInvoices",
+        //     "DocumentLines" => [
+        //         [
+        //             "ItemDescription" => "Test item",
+        //             "Total" => 370000,
+        //             "Price" => 0,
+        //             "AccountCode" => 310003,
+        //             "VatGroup" => "O01",
+        //             "LineTotal" => 370000,
+        //             "RowTotalSC" => 0,
+        //             "TaxCode" => "I1",
+        //             "DiscountPercent" => 0
+        //         ]
+        //     ]
+        // ];
+        // $this->sapService->createSapInvoice($data);
+        dd($request);
+        $data = $request->validate([
+            'patient_type' => 'required',
+            // 'narration' => 'required',
+            'ward' => 'required',
+        ]);
+        $data["attendee"] = Auth::user()->name. " " . Auth::user()->surname;
+        $data["episode_entry"] = (int)Episode::where('patient_id', $request->patient_id)->max('episode_entry') + 1;
+        $data["episode_code"] = $request->patient_id . "/" . $data["episode_entry"];
 
-        // $data["patient_id"] = patient::where('patient_id', $request->patient_id)->first()->id;
-        // $data["date"] = date('Y-m-d');
+        $data["patient_id"] = patient::where('patient_id', $request->patient_id)->first()->id;
+        $data["date"] = date('Y-m-d');
 
-        // try {
-        //     $episode = Episode::create($data);
+        try {
+            $episode = Episode::create($data);
 
-        //     // ChargeSheet::create([
-        //     //     "episode_id" => $episode->id,
-        //     //     "checkin" => date('Y-m-d'),
-        //     // ]);
+            // ChargeSheet::create([
+            //     "episode_id" => $episode->id,
+            //     "checkin" => date('Y-m-d'),
+            // ]);
 
-        //     Payment::create([
-        //         'episode_id' => $episode->id,
-        //         'narration' => $request->narration,
-        //         'amount' => $request->amount,
-        //         'balance' => 0,
-        //         'date' => date('Y-m-d')
-        //     ]);
-        //     return redirect()->back()->with('success', 'payment created successfully');
-        // } catch (\Throwable $th) {
-        //     return redirect()->back()->with('error', $th->getMessage());
-        // }
+            Payment::create([
+                'episode_id' => $episode->id,
+                'narration' => $request->narration,
+                'amount' => $request->amount,
+                'balance' => 0,
+                'date' => date('Y-m-d')
+            ]);
+            return redirect()->back()->with('success', 'payment created successfully');
+        } catch (\Throwable $th) {
+            return redirect()->back()->with('error', $th->getMessage());
+        }
 
 
     }
